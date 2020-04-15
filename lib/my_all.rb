a9c020d730106575a211fc6a16a22fefd755e09e
Neil Empey <argus.two.2@gmail.com>
2020-04-15 16:56:03 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,11 +5,6 @@ def my_all?(collection)
   i = 0 
   while i < collection.length
   yield(collection[i])
-  value = yield(collection[(i + 1) % collection.length])
-  if(yield(collection[i]) != value)
-    return false 
-  end 
   i += 1
 end 
-true
 end
