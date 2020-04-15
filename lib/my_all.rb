5349858cf78a1f38a5a672fbfece97314a306762
Neil Empey <argus.two.2@gmail.com>
2020-04-15 16:58:40 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,12 @@ def my_all?(collection)
   solution = []
   i = 0 
   while i < collection.length
-  yield(collection[i])
+  value = yield(collection[i])
+  solution << value
+  if(solution[i] != solution[(i + 1) % solution.length])
+    return false 
+  end 
   i += 1
 end 
+true  
 end
